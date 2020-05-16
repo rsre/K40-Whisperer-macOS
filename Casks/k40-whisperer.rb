24d9c99eba7053d139e46f0bbe19160b31d8d47f
Ricardo <me@ricardosa.nz>
2020-05-16 06:37:55 +0200

    Update Cask to 0.49
    Removes xquartz requirement since it's no longer necessary for inkscape.

--- Casks/k40-whisperer.rb
+++ Casks/k40-whisperer.rb
@@ -1,6 +1,6 @@
 cask 'k40-whisperer' do
-  version '0.43.B'
-  sha256 '8ab423c5e2ac234c34a2bb12eb6f5e521d9cde934dce57314f4f01eb19046177'
+  version '0.49.A'
+  sha256 '342cadfc883a63495858fa6d36a4f4372c05b35b9de66a6eab019f8aa98acc18'
 
   url "https://github.com/rsre/K40-Whisperer-macOS/releases/download/v#{version}/K40-Whisperer-#{version}.dmg"
   appcast 'https://github.com/rsre/K40-Whisperer-macOS/releases.atom'
@@ -8,7 +8,6 @@ cask 'k40-whisperer' do
   homepage 'https://github.com/rsre/k40-whisperer-macOS'
 
   depends_on formula: 'libusb'
-  depends_on cask: 'xquartz'
   depends_on cask: 'inkscape'
 
   app 'K40 Whisperer.app'
