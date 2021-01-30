8b423e33693a3c2486f0b00940dc9678697ac578
Ricardo <me@ricardosa.nz>
2021-01-30 21:14:10 +0100

    Adds some of stephenhouser recent changes

--- build_macOS.sh
+++ build_macOS.sh
@@ -3,8 +3,10 @@
 # This file executes the build command for the OS X Application bundle.
 # It is here because I am lazy
 # ---------------------------------------------------------------------
+PYTHON_VERSION=3.9.1
 
 # Call getopt to validate the provided input. 
+VENV_DIR=build_env.$$
 VERBOSE=false
 MAKE_DISK=false
 KEEP_VENV=false
@@ -33,53 +35,71 @@ while getopts "hvdesp" OPTION; do
     esac
 done
 
+# Prints the provided error message and then exits with an error code
+function fail {
+    CODE="${1:-1}"
+    MESSAGE="${2:-Unknown error}"
+    echo ""
+    echo -e "\033[31;1;4m*** ERROR: $MESSAGE ***\033[0m"
+    echo ""
+    exit $CODE
+}
+
+
+# Exits with error code/message if the previous command failed
+function check_failure {
+    CODE="$?"
+    MESSAGE="$1"
+    [[ $CODE == 0 ]] || fail "$CODE" "$MESSAGE" 
+}
+
+# *** Not Tested! ***
 if [ "$SETUP_ENVIRONMENT" = true ]
 then
 	# Install HomeBrew (only if you don't have it)
-	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+	/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+	check_failure "Failed to install homebrew"
 
 	# Install Dependencies
-	brew install inkscape
-	brew install libusb
+	brew install --cask inkscape
+	brew install --build-from-source libusb
+	check_failure "Failed to install libusb"
 
-	# Install Python 3.9.1 with tcl-tk using pyenv and set it as the default Python
+	# Tcl/Tk
+	brew install --build-from-source tcl-tk
+	check_failure "Failed to install tcl-tk"
 
-	brew install tcl-tk # install upgraded tcl-tk version
-	brew install pyenv
+	# Install python environments...
+	brew install --build-from-source pyenv
+	check_failure "Failed to install pyenv"
+	eval "$(pyenv init -)"
 
-	pyenv uninstall 3.9.1
-	export PATH="/usr/local/opt/tcl-tk/bin:$PATH"
-	export LDFLAGS="-L/usr/local/opt/tcl-tk/lib"
-	export CPPFLAGS="-I/usr/local/opt/tcl-tk/include"
-	export PKG_CONFIG_PATH="/usr/local/opt/tcl-tk/lib/pkgconfig"
-	export PYTHON_CONFIGURE_OPTS="--with-tcltk-includes='-I$(brew --prefix tcl-tk)/include' \
-                              --with-tcltk-libs='-L$(brew --prefix tcl-tk)/lib -ltcl8.6 -ltk8.6'\
-                              --enable-framework"
-	pyenv install $(pyenv install --list | grep -v - | grep -v b | tail -1)
-	pyenv global 3.9.1
-	pyenv rehash
+	# Install Python with pyenv and set it as the default Python
+	# https://github.com/pyenv/pyenv/issues/94
+	PATH="/usr/local/opt/tcl-tk/bin:$PATH" \
+		LDFLAGS="-L/usr/local/opt/tcl-tk/lib" \
+		CPPFLAGS="-I/usr/local/opt/tcl-tk/include" \
+		PKG_CONFIG_PATH="/usr/local/opt/tcl-tk/lib/pkgconfig" \
+		PYTHON_CONFIGURE_OPTS="--enable-framework --with-tcltk-includes='-I$(brew --prefix tcl-tk)/include' --with-tcltk-libs='-L$(brew --prefix tcl-tk)/lib -ltcl8.6 -ltk8.6'" \
+		pyenv install ${PYENV_PYTHON_VERSION}
+	check_failure "Failed to install Python ${PYENV_PYTHON_VERSION}"
+
+	# Select Python to use
+	pyenv local ${PYENV_PYTHON_VERSION} && pyenv rehash
+	check_failure "Failed to setup Python ${PYENV_PYTHON_VERSION}"
 fi
 
 echo "Validate environment..."
+OS=$(uname)
+if [ "${OS}" != "Darwin" ]; then
+	fail "Um... this build script is for OSX/macOS."
+fi
 
-# Get version from main source file.
-VERSION=$(grep "^version " k40_whisperer.py | grep -Eo "[\.0-9]+")
-
-# Determine Python to use... prefer Python3
+# Use the specific python version from pyenv so we don't get hung up on the
+# system python or a user's own custom environment.
 PYTHON=$(command -v python3)
-if [ -z "${PYTHON}" ]
-then
-	PYTHON=$(command -v python)
-fi
-
-# Upgrade pip version
-${PYTHON} -m pip install --upgrade pip
-
-PIP=$(command -v pip3)
-if [ -z "${PIP}" ]
-then
-	PIP=$(command -v pip)
-fi
+PY_VER=$($PYTHON --version 2>&1 | awk '{ print $2 }')
+[[ ${PY_VER} == "${PYTHON_VERSION}" ]] || fail 1 "Packaging REQUIRES Python ${PYTHON_VERSION}. Please rerun with -s to setup build environment"
 
 # Clean up any previous build work
 echo "Remove old builds..."
@@ -87,23 +107,25 @@ rm -rf ./build ./dist *.pyc ./__pycache__
 
 # Set up and activate virtual environment for dependencies
 echo "Setup Python Virtual Environment..."
-PY_VER=$(${PYTHON} --version 2>&1)
-if [[ $PY_VER == *"2.7"* ]]
-then
-	${PIP} install virtualenv py2app==0.16
-	virtualenv python_venv
-else
-	${PYTHON} -m venv python_venv
-fi
+python3 -m venv "${VENV_DIR}"
+check_failure "Failed to initialize python venv"
 
-source ./python_venv/bin/activate
+source "./${VENV_DIR}/bin/activate"
+check_failure "Failed to activate python venv"
+
+# Unset our python variable now that we are running inside of the virtualenv
+# and can just use `python` directly
+PYTHON=
 
 # Install requirements
 echo "Install Dependencies..."
-${PIP} install -r requirements.txt
+python3 -m pip install --upgrade pip
+pip3 install -r requirements.txt
+check_failure "Failed to install python requirements"
 
 echo "Build macOS Application Bundle..."
 
+# Create .spec file if it doesn't exist
 FILE=k40_whisperer.spec
 if [ -f "$FILE" ]; then
 	${PYTHON} -O -m PyInstaller -y --clean k40_whisperer.spec
@@ -128,10 +150,17 @@ else
     ${PYTHON} -O -m PyInstaller -y --clean k40_whisperer.spec
 fi
 
+# Get version from main source file.
+VERSION=$(grep "^version " k40_whisperer.py | grep -Eo "[\.0-9]+")
+
+python3 -OO -m PyInstaller -y --clean k40_whisperer.spec
+check_failure "Failed to package k40_whisperer bundle"
+
+# Remove temporary binary
 rm -rf dist/k40_whisperer
 
 echo "Copy support files to dist..."
-cp k40_whisperer_test.svg Change_Log.txt gpl-3.0.txt dist
+cp k40_whisperer_test.svg Change_Log.txt gpl-3.0.txt README.md dist
 
 # Clean up the build directory when we are done.
 echo "Clean up build artifacts..."
@@ -142,16 +171,17 @@ if [ "$KEEP_VENV" = false ]
 then
 	echo "Remove Python virtual environment..."
 	deactivate
-	rm -rf python_venv
+	rm -rf "${VENV_DIR}"
 fi
 
 # Buid a new disk image
 if [ "$MAKE_DISK" = true ]
 then
 	echo "Build macOS Disk Image..."
-
 	VOLNAME=K40-Whisperer-${VERSION}
+	rm ${VOLNAME}.dmg
 	hdiutil create -fs HFS+ -volname ${VOLNAME} -srcfolder ./dist ./${VOLNAME}.dmg
+	check_failure "Failed to build k40_whisperer dmg"
 	mv ${VOLNAME}.dmg ./dist
 fi
 
